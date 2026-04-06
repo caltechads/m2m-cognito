60cd4264ca07627fd0ed6071622bc3c06ca6eee6
Glenn Bach <glenn@caltech.edu>
2026-04-05 18:39:25 -0700

    Bump version: 0.1.1 -> 0.1.2

--- m2m_cognito/__init__.py
+++ m2m_cognito/__init__.py
@@ -1,6 +1,6 @@
 """Machine-to-machine Amazon Cognito OAuth2 client credentials and token validation."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from m2m_cognito.client import CognitoM2MClient, TokenResponse
 from m2m_cognito.exceptions import (
